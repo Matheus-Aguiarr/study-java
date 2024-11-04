e2f9967cc238e727e5d4a62406e5e6556f89a0fe
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-11-04 19:11:46 -0300

    o que e um objeto? java

---
+++ aula001/Main.java
@@ -0,0 +1,35 @@
+// What is an object
+/*
+----- Um objeto eh uma coisa material ou abstrata que pode ser percebida pelos sentidos e descrita por meio de suas caracteristicas, comportamentos e estado atual. ----------------
+
+    Objeto - no caso de uma caneta, seria a caneta em si.
+    Classe - seria o molde da caneta. Existem canetas (objeto) de diversas cores, porém, o molde (classe) é sempre o mesmo.
+
+    Atributos - O que eu tenho (caracteristicas)
+    Métodos - O que eu faço (comportamentos)
+    Estado - Estado atual (status)
+
+    Exemplo caneta => {
+        Atributos - Modelo, cor, ponta, carga, tampada
+        Métodos - Escrever, rabiscar, pintar, tampar
+        Estado - 50% de carga da tinta
+    }
+    Instanciar - pegar uma classe e gerar um objeto.
+        c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5;
+
+        c2 = new Caneta();
+        c2.cor = "Vermelho";
+        c2.ponta = 1.0;
+    Nesse caso, c1 e c2 são diferentes objetos mas que vem de uma mesma classe (de um mesmo molde)
+
+    Abstração - Abstrair aquilo que não me é util;
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
+
