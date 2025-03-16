8b7059adc9f199e2911cba291c3c6b84f107996d
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2025-03-16 00:13:34 -0300

    Comecei a fazer o screen match do curso da alura

---
+++ screen-match/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        String name = "Vingadores Ultimato";
+        int ano = 2019;
+        System.out.println("Ano de lancamento: " + ano);
+        boolean incluidoNoPlano = true;
+        String isIncluded = incluidoNoPlano ? "It's included" : "It's not included";
+
+        System.out.println(String.format("The name of the movie is %s, it was made in %d and %s on the plan", name, ano, isIncluded));
+
+        double media = (9.0 + 8.0 + 10.0) /3;
+        System.out.println("Media: " + media);
+
+        int classificacao = (int) (media /2); // Conversao do valor de double pra int
+        System.out.println("Classificacao " + classificacao);
+        for(int i = 0; i <= classificacao; i++) {
+            System.out.println("#");
+        }
+
+
+    }
+}
